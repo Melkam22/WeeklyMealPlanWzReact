fb7add7957d3bfb27602c5119c6c4f453b21a92b
Ashu <ashenshi@gmail.com>
2020-05-03 14:22:21 +0200

    addEditDelete function added & linked to localstorage

--- src/resource/mealList.js
+++ src/resource/mealList.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 import MealForm from './mealForm';
 
 class MealList extends Component {
+    state = {
+        meal : this.mealFunction(),
+        currentIndex: -1,
+    }
+
+    mealFunction(){
+        if (localStorage.getItem('mealPlan') == null)
+            localStorage.setItem('mealPlan', JSON.stringify([]));
+            return JSON.parse(localStorage.getItem ('mealPlan'))
+    }
+
+    addEditDelete = (data) =>{
+        let meal = this.mealFunction();
+        if(this.state.currentIndex == -1)//nothing there, add data
+        meal.push(data)
+            else meal[this.state.currentIndex] = data//the current data remains the same
+            localStorage.setItem('mealPlan', JSON.stringify(meal))
+            this.setState({
+                meal: meal, 
+                currentIndex: -1
+            })
+    }
 
   render(){
   return (
       <div>
            <h1>MealList</h1>
-        <MealForm />
+        <MealForm addEditDelete={this.addEditDelete}/>
 
         <hr />
     
