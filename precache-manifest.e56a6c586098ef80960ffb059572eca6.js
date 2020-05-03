022c868f46d94c0bb40f946129bbfcaf71041837
Ashu <ashenshi@gmail.com>
2020-05-03 12:48:53 +0200

    Updates

---
+++ precache-manifest.e56a6c586098ef80960ffb059572eca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5416ce71b8d188f1c057e6f06f6040ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eea0151735a2513314b3",
+    "url": "./static/css/main.df85431d.chunk.css"
+  },
+  {
+    "revision": "eca53639d717325e2275",
+    "url": "./static/js/2.0f14c2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f14c2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea0151735a2513314b3",
+    "url": "./static/js/main.de67b121.chunk.js"
+  },
+  {
+    "revision": "e857575efdddcb99e873",
+    "url": "./static/js/runtime-main.d16e3db3.js"
+  }
+]);
